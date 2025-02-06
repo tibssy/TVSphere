3612501bfc179265fa6d3f7ed159121f1048633d
tibssy <norbert.tiborcz@gmail.com>
2025-02-06 02:12:02 +0000

    Remove titlebar and set colors

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import flet_video as ftv
 class VideoPlayer(ft.Container):
     DEFAULT_MARGIN = ft.Margin(left=200, right=15, top=60, bottom=15)
     FULLSCREEN_MARGIN = ft.Margin(left=0, right=0, top=0, bottom=0)
-    DEFAULT_BORDER_RADIUS = 12
+    DEFAULT_BORDER_RADIUS = 16
     FULLSCREEN_BORDER_RADIUS = 0
     SIZES = ['contain', 'cover', 'fill', 'fitHeight', 'fitWidth', 'scaleDown']
 
@@ -17,12 +17,12 @@ class VideoPlayer(ft.Container):
         self.margin = self.DEFAULT_MARGIN
         self.border_radius = self.DEFAULT_BORDER_RADIUS
         self.animate = ft.Animation(400, ft.AnimationCurve.EASE)
-        self.bgcolor = ft.Colors.BLACK
-        self.video_player = self.create_video_player()
+        self.bgcolor = ft.Colors.GREY_900
+        self.video_player = self._create_video_player()
         self.shadow = ft.BoxShadow(
             spread_radius=1,
             blur_radius=6,
-            color=ft.colors.SHADOW,
+            color=ft.Colors.SHADOW,
             offset=ft.Offset(-1, 3)
         )
         self.content = ft.Container(
@@ -30,7 +30,7 @@ class VideoPlayer(ft.Container):
             on_click=self.toggle_fullscreen
         )
 
-    def create_video_player(self):
+    def _create_video_player(self):
         return ftv.Video(
             playlist=[
                 ftv.VideoMedia(
@@ -38,14 +38,13 @@ class VideoPlayer(ft.Container):
                 )
             ],
             playlist_mode=ftv.PlaylistMode.SINGLE,
-            fill_color='#202020',
+            fill_color=ft.Colors.GREY_900,
             fit=ft.ImageFit(self.SIZES[self.fit]),
             autoplay=True,
             show_controls=False,
             wakelock=True,
             on_loaded=lambda e: print("Video loaded successfully!")
         )
-
 
     def toggle_fullscreen(self, e):
         self.fullscreen = not self.fullscreen
@@ -67,8 +66,8 @@ class VideoPlayer(ft.Container):
         self.video_player.update()
 
     @classmethod
-    def set_margin(cls):
-        cls.DEFAULT_MARGIN = ft.Margin(left=320, right=20, top=70, bottom=20)
+    def set_margin(cls, margin: ft.Margin):
+        cls.DEFAULT_MARGIN = margin
 
     def update_margin(self):
         self.margin = self.DEFAULT_MARGIN
@@ -97,14 +96,22 @@ class SideBar(ft.Stack):
                             on_click=self.show_sub_container
 
                         ),
-                        bgcolor=ft.Colors.DEEP_ORANGE_900,
+                        # bgcolor=ft.Colors.DEEP_ORANGE_900,
                         padding=10,
                         width=self.width,
-                        height=self.header_height,
+                        height=self.header_height
                     ),
-                ]
+                    ft.Divider(
+                        height=3,
+                        thickness=1,
+                        color=ft.Colors.ORANGE_100,
+                        leading_indent=12,
+                        trailing_indent=12
+                    ),
+                ],
+                spacing=0
             ),
-            bgcolor=ft.Colors.ORANGE,
+            bgcolor=ft.Colors.GREY_800,
             expand=True,
             offset=ft.Offset(x=0, y=0),
             animate_offset = ft.Animation(400, ft.AnimationCurve.EASE)
@@ -112,7 +119,7 @@ class SideBar(ft.Stack):
 
     def create_sub_conatainer(self):
         return ft.Container(
-            bgcolor=ft.Colors.BLUE,
+            # bgcolor=ft.Colors.BLUE,
             expand=True
         )
 
@@ -122,18 +129,24 @@ class SideBar(ft.Stack):
 
 
 class HeaderBar(ft.Container):
-    def __init__(self, header_height, **kwargs):
+    def __init__(self, header_height, platform, **kwargs):
         super().__init__(**kwargs)
+        self.platform = platform
         self.padding = ft.padding.only(right=20)
         self.height = header_height
-        self.bgcolor = ft.Colors.PURPLE
+        # self.bgcolor = ft.Colors.PURPLE
         self.content = ft.Row(
             controls = [
-                ft.Container(bgcolor=ft.Colors.PURPLE, expand=True),
+                ft.WindowDragArea(
+                    ft.Container(
+                        # bgcolor=ft.Colors.GREEN_600,
+                        expand=True
+                    ),
+                    expand=True
+                ),
                 self.create_app_controls()
             ]
         )
-
 
     def create_app_controls(self):
         icon_style = ft.ButtonStyle(
@@ -141,23 +154,22 @@ class HeaderBar(ft.Container):
             padding=0,
             icon_size=26
         )
-        return ft.Row(
-            controls=[
-                ft.IconButton(ft.Icons.REPEAT_ROUNDED, style=icon_style, on_click=lambda e: print('REPEAT_ROUNDED')),
-                ft.IconButton(ft.Icons.ASPECT_RATIO, style=icon_style, on_click=lambda e: e.page.overlay[0].change_ratio(e)),
-                ft.IconButton(ft.Icons.TIMER_OUTLINED, style=icon_style, on_click=lambda e: print('TIMER')),
-                ft.IconButton(ft.Icons.FULLSCREEN, style=icon_style, on_click=self.toggle_fullscreen),
-                ft.IconButton(ft.Icons.CLOSE, style=icon_style, on_click=self.close_app)
-            ],
-            spacing=0
-        )
+        controls = [
+            ft.IconButton(ft.Icons.REPEAT_ROUNDED, style=icon_style, on_click=lambda e: print('REPEAT_ROUNDED')),
+            ft.IconButton(ft.Icons.ASPECT_RATIO, style=icon_style, on_click=lambda e: e.page.overlay[0].change_ratio(e)),
+            ft.IconButton(ft.Icons.TIMER_OUTLINED, style=icon_style, on_click=lambda e: print('TIMER')),
+            ft.IconButton(ft.Icons.CLOSE, style=icon_style, on_click=self.close_app)
+        ]
+        if self.platform != 'android':
+            controls.insert(-1, ft.IconButton(ft.Icons.FULLSCREEN, style=icon_style, on_click=self.toggle_fullscreen))
+        return ft.Row(controls=controls, spacing=0)
 
     def toggle_fullscreen(self, e):
         self.page.window.full_screen = not self.page.window.full_screen
         self.page.update()
 
     def close_app(self, e):
-        if self.page.platform.value == 'android':
+        if self.platform == 'android':
             import os
             os._exit(0)
         else:
@@ -166,13 +178,14 @@ class HeaderBar(ft.Container):
 class TVSphereApp:
     def __init__(self, page: ft.Page):
         self.page = page
+        self.platform = self.page.platform.value
         self.video_player = VideoPlayer()
         self.sidebar_width = self.calculate_sidebar_width()
         self.header_height = self.calculate_header_height()
         self.setup_page()
 
     def calculate_sidebar_width(self):
-        return 320 if self.page.width > 768 else 200
+        return 360 if self.page.width > 768 else 200
 
     def calculate_header_height(self):
         return 70 if self.page.width > 768 else 60
@@ -184,9 +197,13 @@ class TVSphereApp:
         self.adjust_video_player_margin()
 
     def configure_page_properties(self):
+        self.page.window.title_bar_hidden = True
+        self.page.window.title_bar_buttons_hidden = True
         self.page.title = 'TVSphere'
         self.page.padding = 0
         self.page.spacing = 0
+        self.page.bgcolor = ft.Colors.GREY_800
+        self.page.theme = ft.Theme(color_scheme_seed=ft.Colors.ORANGE_ACCENT)
 
     def add_video_player_to_overlay(self):
         self.page.overlay.append(self.video_player)
@@ -197,7 +214,7 @@ class TVSphereApp:
         )
 
         sidebar_container = SideBar(width=self.sidebar_width, header_height=self.header_height)
-        header_bar = HeaderBar(header_height=self.header_height)
+        header_bar = HeaderBar(header_height=self.header_height, platform=self.platform)
         self.page.add(
             ft.Row(
                 controls=[
@@ -216,7 +233,7 @@ class TVSphereApp:
 
     def adjust_video_player_margin(self):
         if self.page.width > 768:
-            self.video_player.set_margin()
+            self.video_player.set_margin(ft.Margin(left=self.sidebar_width, right=20, top=self.header_height, bottom=20))
             self.video_player.update_margin()
 
 
